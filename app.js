b6d08a229190364b569f98a23c641d094d451195
Irving Diaz <irvingedc@gmail.com>
2017-12-26 02:28:56 -0500

    completed scraping in all stores

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var index = require('./routes/index');
 
 var general = require('./routes/general');
 var google = require('./routes/google');
+var googleDetails = require('./routes/googleDetails');
 
 var app = express();
 app.use(cors());
@@ -26,11 +27,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-let mainRoute = "/scrapers/"
+let mainRoute = "/aicon-scrapers/"
 
 app.use(mainRoute, index);
 app.use(mainRoute+'general', general);
 app.use(mainRoute+'google', google);
+app.use(mainRoute+'googleDetails', googleDetails);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
