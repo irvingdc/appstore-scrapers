f5bae5111b5a818362e6fc223e055e913a6eedee
Irving Diaz <irvingedc@gmail.com>
2018-04-09 01:20:49 -0400

    adjusted environment variables

--- routes/amazonDetails.js
+++ routes/amazonDetails.js
@@ -7,17 +7,21 @@ async function run(req, res, next) {
 		page = await global.browser.newPage()
 		let url = decodeURI(req.query.url)
 		await page.goto(url,{timeout:10000})
+		//await page.waitForSelector(".buying a",{timeout:10000})
 		var result = await page.evaluate(() => { 
 			let aEmail = Array.from(document.querySelectorAll("#mas-developer-info a")).find(it => { return it.href.includes("mailto") })
-			let aWebsite = Array.from(document.querySelectorAll("#mas-developer-info a")).find(it => { return it.href.includes("http") })
+			let aWebsite = Array.from(document.querySelectorAll("#mas-developer-info a"))
+								.filter(it => { return it.href.includes("http") })
+								.find(it => { return !it.innerHTML.includes("More apps") })
 			let email = aEmail ? aEmail.href.split(":")[1] : ""
-			let company = document.querySelector(".buying a") ? document.querySelector(".buying a").innerHTML : ""
+			let company = Array.from(document.querySelectorAll(".buying a")).map(a=>{return a.innerHTML})[0]
 			let website = aWebsite ? aWebsite.href : ""
 			return { email:email, company:company, website:website}
 		})
 		page.close()
 		console.log(result)
 		return {id:req.query.id, ...result}
+		//return result
 	}
 	catch(e){
 		page.close()
@@ -32,4 +36,3 @@ router.get('/', async (req, res, next) => {
 })
 
 module.exports = router
-
